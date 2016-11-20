3558dae2e6d3f68f62c11021872456e3d2578052
Shi Tuocheng <shituocheng@126.com>
2016-11-20 15:59:54 +0800

    增加封装fragment

---
+++ app/src/main/java/com/shituocheng/calcalculateapplication/com/baseactivity/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.shituocheng.calcalculateapplication.com.baseactivity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by shituocheng on 2016/11/20.
+ */
+
+public class BaseFragment extends Fragment {
+}
